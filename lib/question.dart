9f2148700213a8aae49d6244ad82d4209ddf0467
Gabriel Souza <gabrielss_@outlook.com>
2021-03-31 22:34:22 -0300

    att

--- lib/question.dart
+++ lib/question.dart
@@ -19,20 +19,22 @@ class Question {
     List<Question> questions = [];
 
     questions.add(Question(
-        questionText: 'O que é Flutter?',
-        option1: 'Um novo smartphone.',
-        option2: 'Um sistema operacional.',
-        option3: 'Um SDK de desenvolvimento multiplataforma.',
-        option4: 'Uma linguagem de programação orientada a objetos.',
-        answer: 3));
+      questionText: 'O que é Flutter?',
+      option1: 'Um novo smartphone.',
+      option2: 'Um sistema operacional.',
+      option3: 'Um SDK de desenvolvimento multiplataforma.',
+      option4: 'Uma linguagem de programação orientada a objetos.',
+      answer: 3,
+    ));
 
     questions.add(Question(
-        questionText: 'Qual linguagem de programação é usada pelo Flutter?',
-        option1: 'Dart',
-        option2: 'Java',
-        option3: 'JavaScript',
-        option4: 'Go',
-        answer: 1));
+      questionText: 'Qual linguagem de programação é usada pelo Flutter?',
+      option1: 'Dart',
+      option2: 'Java',
+      option3: 'JavaScript',
+      option4: 'Go',
+      answer: 1,
+    ));
 
     return questions;
   }
